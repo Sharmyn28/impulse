c304e5067258bee4cc8c0e348ec896a51d4c2335
Milagros Rodriguez <sharmynrodriguez@gmail.com>
2019-04-01 08:56:48 -0500

    fixing and reordering files and folders

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# impulse
-Reto técnico sobre cuidado de piel para Impulse
+Este proyecto fue maquetado usando librerías para CSS como [Bootstrap](https://getbootstrap.com/) y [Owl Carousel](https://owlcarousel2.github.io/OwlCarousel2/).
+
+A continuación encontrarás información acerca del funcionamiento y recomendaciones para el proyecto.
+
+
+# Impulse - Tu Piel tiene memoria
+
+Reto técnico Front end para Impulse, landing page sobre cuidado de piel y prevención del cancer de piel.
+
+
+## Tabla de Contenidos
+
+- [Updating to New Releases](#updating-to-new-releases)
+- [Sending Feedback](#sending-feedback)
+- [Folder Structure](#folder-structure)
+- [Available Scripts](#available-scripts)
+  - [npm start](#npm-start)
+  - [npm test](#npm-test)
+  - [npm run build](#npm-run-build)
+  - [npm run eject](#npm-run-eject)
+- [Supported Language Features and Polyfills](#supported-language-features-and-polyfills)
+- [Syntax Highlighting in the Editor](#syntax-highlighting-in-the-editor)
+- [Displaying Lint Output in the Editor](#displaying-lint-output-in-the-editor)
+- [Recomendaciones](#Recomendaciones)
+
+## Recomendaciones
--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-$('.owl-carousel').owlCarousel({
+
+// Se inicializa libreria OwlCarousel para el tema 2
+$('#theme2-carousel').owlCarousel({
   loop: true,
   margin: 10,
   center: true,
@@ -21,3 +23,177 @@ $('.owl-carousel').owlCarousel({
     }
   }
 })
+
+// se inicializa libreria OwlCarousel para tema 1
+$("#theme1-slider").owlCarousel({
+  autoWidth:true,
+  items:6
+});
+
+document.getElementById("tono-1").onclick = enableInfo1;
+function enableInfo1() {
+  document.getElementById("item-top2").classList.remove("change-item-top");
+  document.getElementById("item-top3").classList.remove("change-item-top");
+  document.getElementById("item-top4").classList.remove("change-item-top");
+  document.getElementById("item-top5").classList.remove("change-item-top");
+  document.getElementById("item-top6").classList.remove("change-item-top");
+  document.getElementById("item-top1").classList.add("change-item-top");
+  document.getElementById("number2").classList.remove("enable-info");
+  document.getElementById("number3").classList.remove("enable-info");
+  document.getElementById("number4").classList.remove("enable-info");
+  document.getElementById("number5").classList.remove("enable-info");
+  document.getElementById("number6").classList.remove("enable-info");
+  document.getElementById("number1").classList.add("enable-info");
+  document.getElementById("image-6").classList.remove("changes-info");
+  document.getElementById("image-2").classList.remove("changes-info");
+  document.getElementById("image-3").classList.remove("changes-info");
+  document.getElementById("image-4").classList.remove("changes-info");
+  document.getElementById("image-5").classList.remove("changes-info");
+  document.getElementById("image-1").classList.add("changes-info");
+  document.getElementById("tono-6").classList.remove("changes-tono");
+  document.getElementById("tono-2").classList.remove("changes-tono");
+  document.getElementById("tono-3").classList.remove("changes-tono");
+  document.getElementById("tono-4").classList.remove("changes-tono");
+  document.getElementById("tono-5").classList.remove("changes-tono");
+  document.getElementById("tono-1").classList.add("changes-tono");
+}
+document.getElementById("tono-2").onclick = enableInfo2;
+function enableInfo2() {
+  document.getElementById("item-top1").classList.remove("change-item-top");
+  document.getElementById("item-top3").classList.remove("change-item-top");
+  document.getElementById("item-top4").classList.remove("change-item-top");
+  document.getElementById("item-top5").classList.remove("change-item-top");
+  document.getElementById("item-top6").classList.remove("change-item-top");
+  document.getElementById("item-top2").classList.add("change-item-top");
+  document.getElementById("number1").classList.remove("enable-info");
+  document.getElementById("number3").classList.remove("enable-info");
+  document.getElementById("number4").classList.remove("enable-info");
+  document.getElementById("number5").classList.remove("enable-info");
+  document.getElementById("number6").classList.remove("enable-info");
+  document.getElementById("number2").classList.add("enable-info");
+  document.getElementById("image-1").classList.remove("changes-info");
+  document.getElementById("image-6").classList.remove("changes-info");
+  document.getElementById("image-3").classList.remove("changes-info");
+  document.getElementById("image-4").classList.remove("changes-info");
+  document.getElementById("image-5").classList.remove("changes-info");
+  document.getElementById("image-2").classList.add("changes-info");
+  document.getElementById("tono-1").classList.remove("changes-tono");
+  document.getElementById("tono-6").classList.remove("changes-tono");
+  document.getElementById("tono-3").classList.remove("changes-tono");
+  document.getElementById("tono-4").classList.remove("changes-tono");
+  document.getElementById("tono-5").classList.remove("changes-tono");
+  document.getElementById("tono-2").classList.add("changes-tono");
+}
+document.getElementById("tono-3").onclick = enableInfo3;
+function enableInfo3() {
+  document.getElementById("item-top1").classList.remove("change-item-top");
+  document.getElementById("item-top2").classList.remove("change-item-top");
+  document.getElementById("item-top4").classList.remove("change-item-top");
+  document.getElementById("item-top5").classList.remove("change-item-top");
+  document.getElementById("item-top6").classList.remove("change-item-top");
+  document.getElementById("item-top3").classList.add("change-item-top");
+  document.getElementById("number1").classList.remove("enable-info");
+  document.getElementById("number2").classList.remove("enable-info");
+  document.getElementById("number4").classList.remove("enable-info");
+  document.getElementById("number5").classList.remove("enable-info");
+  document.getElementById("number6").classList.remove("enable-info");
+  document.getElementById("number3").classList.add("enable-info");
+  document.getElementById("image-1").classList.remove("changes-info");
+  document.getElementById("image-2").classList.remove("changes-info");
+  document.getElementById("image-6").classList.remove("changes-info");
+  document.getElementById("image-4").classList.remove("changes-info");
+  document.getElementById("image-5").classList.remove("changes-info");
+  document.getElementById("image-3").classList.add("changes-info");
+  document.getElementById("tono-1").classList.remove("changes-tono");
+  document.getElementById("tono-2").classList.remove("changes-tono");
+  document.getElementById("tono-6").classList.remove("changes-tono");
+  document.getElementById("tono-4").classList.remove("changes-tono");
+  document.getElementById("tono-5").classList.remove("changes-tono");
+  document.getElementById("tono-3").classList.add("changes-tono");
+}
+document.getElementById("tono-4").onclick = enableInfo4;
+function enableInfo4() {
+  document.getElementById("item-top1").classList.remove("change-item-top");
+  document.getElementById("item-top2").classList.remove("change-item-top");
+  document.getElementById("item-top3").classList.remove("change-item-top");
+  document.getElementById("item-top5").classList.remove("change-item-top");
+  document.getElementById("item-top6").classList.remove("change-item-top");
+  document.getElementById("item-top4").classList.add("change-item-top");
+  document.getElementById("number1").classList.remove("enable-info");
+  document.getElementById("number2").classList.remove("enable-info");
+  document.getElementById("number3").classList.remove("enable-info");
+  document.getElementById("number5").classList.remove("enable-info");
+  document.getElementById("number6").classList.remove("enable-info");
+  document.getElementById("number4").classList.add("enable-info");
+  document.getElementById("image-1").classList.remove("changes-info");
+  document.getElementById("image-2").classList.remove("changes-info");
+  document.getElementById("image-3").classList.remove("changes-info");
+  document.getElementById("image-6").classList.remove("changes-info");
+  document.getElementById("image-5").classList.remove("changes-info");
+  document.getElementById("image-4").classList.add("changes-info");
+  document.getElementById("tono-1").classList.remove("changes-tono");
+  document.getElementById("tono-2").classList.remove("changes-tono");
+  document.getElementById("tono-3").classList.remove("changes-tono");
+  document.getElementById("tono-6").classList.remove("changes-tono");
+  document.getElementById("tono-5").classList.remove("changes-tono");
+  document.getElementById("tono-4").classList.add("changes-tono");
+}
+document.getElementById("tono-5").onclick = enableInfo5;
+function enableInfo5() {
+  document.getElementById("item-top1").classList.remove("change-item-top");
+  document.getElementById("item-top2").classList.remove("change-item-top");
+  document.getElementById("item-top3").classList.remove("change-item-top");
+  document.getElementById("item-top4").classList.remove("change-item-top");
+  document.getElementById("item-top6").classList.remove("change-item-top");
+  document.getElementById("item-top5").classList.add("change-item-top");
+  document.getElementById("number1").classList.remove("enable-info");
+  document.getElementById("number2").classList.remove("enable-info");
+  document.getElementById("number3").classList.remove("enable-info");
+  document.getElementById("number4").classList.remove("enable-info");
+  document.getElementById("number6").classList.remove("enable-info");
+  document.getElementById("number5").classList.add("enable-info");
+  document.getElementById("image-1").classList.remove("changes-info");
+  document.getElementById("image-2").classList.remove("changes-info");
+  document.getElementById("image-3").classList.remove("changes-info");
+  document.getElementById("image-4").classList.remove("changes-info");
+  document.getElementById("image-6").classList.remove("changes-info");
+  document.getElementById("image-5").classList.add("changes-info");
+  document.getElementById("tono-1").classList.remove("changes-tono");
+  document.getElementById("tono-2").classList.remove("changes-tono");
+  document.getElementById("tono-3").classList.remove("changes-tono");
+  document.getElementById("tono-4").classList.remove("changes-tono");
+  document.getElementById("tono-6").classList.remove("changes-tono");
+  document.getElementById("tono-5").classList.add("changes-tono");
+}
+document.getElementById("tono-6").onclick = enableInfo6;
+function enableInfo6() {
+  document.getElementById("item-top1").classList.remove("change-item-top");
+  document.getElementById("item-top2").classList.remove("change-item-top");
+  document.getElementById("item-top3").classList.remove("change-item-top");
+  document.getElementById("item-top4").classList.remove("change-item-top");
+  document.getElementById("item-top5").classList.remove("change-item-top");
+  document.getElementById("item-top6").classList.add("change-item-top");
+  document.getElementById("number1").classList.remove("enable-info");
+  document.getElementById("number2").classList.remove("enable-info");
+  document.getElementById("number3").classList.remove("enable-info");
+  document.getElementById("number4").classList.remove("enable-info");
+  document.getElementById("number5").classList.remove("enable-info");
+  document.getElementById("number6").classList.add("enable-info");
+  document.getElementById("image-1").classList.remove("changes-info");
+  document.getElementById("image-2").classList.remove("changes-info");
+  document.getElementById("image-3").classList.remove("changes-info");
+  document.getElementById("image-4").classList.remove("changes-info");
+  document.getElementById("image-5").classList.remove("changes-info");
+  document.getElementById("image-6").classList.add("changes-info");
+  document.getElementById("tono-1").classList.remove("changes-tono");
+  document.getElementById("tono-2").classList.remove("changes-tono");
+  document.getElementById("tono-3").classList.remove("changes-tono");
+  document.getElementById("tono-4").classList.remove("changes-tono");
+  document.getElementById("tono-5").classList.remove("changes-tono");
+  document.getElementById("tono-6").classList.add("changes-tono");
+}
+
+// al crear documento se inicializa con Tipo 3 de piel acitvado en el carousel
+$('document').ready(function(){
+  enableInfo3()
+});
